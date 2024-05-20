32ddb69a13c5d283d3a0c1bc594c8219eb6f128c
Mikhail Shornikov <mikhail.shornikov@mail.ru>
2024-05-21 01:12:19 +0300

    impr(frontend): Улучшает стили авторизации

--- frontend/js/chatsPage.js
+++ frontend/js/chatsPage.js
@@ -48,6 +48,11 @@ const chatsPage = () => {
                 link.classList.add("chats-list__item");
                 link.href = `/chat/${i.username}`;
                 link.innerText = i.username;
+                link.innerHTML += `
+                <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="gray" style="display: block">
+                <path
+                    d="M504-480 348-636q-11-11-11-28t11-28q11-11 28-11t28 11l184 184q6 6 8.5 13t2.5 15q0 8-2.5 15t-8.5 13L404-268q-11 11-28 11t-28-11q-11-11-11-28t11-28l156-156Z" />
+                </svg>`;
                 item.appendChild(link);
                 list.appendChild(item);
                 console.log(item);
